b0c61ade29026b164946b00f6a669442114d408f
kalaider <a.kalaider@yandex.ru>
2017-11-23 01:34:41 +0300

    Main logic implemented

--- src/wave-packet/WavePacketDlg.cpp
+++ src/wave-packet/WavePacketDlg.cpp
@@ -9,6 +9,8 @@
 
 #include <util/common/iterable.h>
 
+#include "model.h"
+
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
@@ -32,6 +34,8 @@ plot_t wave_packet_plot
 
 
 world_t::ptr_t wavefunc_fixed_bound = world_t::create();
+
+std::vector < double > energy_levels;
 
 CWavePacketDlg::CWavePacketDlg(CWnd* pParent /*=NULL*/)
 	: CSimulationDialog(CWavePacketDlg::IDD, pParent)
@@ -284,6 +288,289 @@ HCURSOR CWavePacketDlg::OnQueryDragIcon()
 
 void CWavePacketDlg::OnSimulation()
 {
+    auto & wave_packet = * wave_packet_plot.data->container_ptr;
+    auto & spectrum    = * spectrum_plot.data->container_ptr;
+
+    const size_t max_energy_levels = max_wavefuncs;
+
+    std::vector < std::vector < complex < double > > > wave_packets;
+
+    // fill wave packet with default data
+
+    size_t n = std::ceil(m_lfModelingInterval / m_lfDistanceDelta);
+
+    wavefunc_fixed_bound->xmin = 0;
+    wavefunc_fixed_bound->xmax = m_lfModelingInterval;
+
+    wave_packet.resize(n);
+    for (size_t i = 0; i < wave_packet.size(); ++i)
+    {
+        double x = i * m_lfDistanceDelta;
+        wave_packet[i] = m_lfPacketMagnitude * exp(- (x - m_lfPacketPosition) * (x - m_lfPacketPosition) / m_lfPacketDispersion / m_lfPacketDispersion);
+    }
+
+    // make potential and boundary condition functions
+
+    auto p = model::make_potential_fn(m_lfBarrierWidth, m_lfBarrierHeight, m_nOrbitalMomentum);
+    auto s = model::make_free_space_condition_fn((m_lfBarrierWidth + m_lfModelingInterval) / 2, m_lfGamma);
+
+    // init common data
+
+    tmm_data data;
+    tmm_make_data(data, m_lfTimeDelta, 0, m_lfModelingInterval, m_lfDistanceDelta, p, s);
+
+    size_t fixed_point = rand() % n;
+
+    // do while not interrupted
+
+    wave_packet_plot.refresh();
+    m_cWavePacketPlot.RedrawBuffer();
+    m_cWavePacketPlot.SwapBuffers();
+    Invoke([&] () {
+        m_cWavePacketPlot.RedrawWindow();
+    });
+
+    energy_levels.clear();
+    std::vector < size_t > energy_level_indices;
+
+    size_t t = 0;
+
+    while (m_bWorking)
+    {
+        // calculate next wave packet state
+
+        tmm_solve(data, wave_packet, wave_packet, 0, 0, 0, 0);
+
+        // if enough time passed, start recording wave packet
+        // state at the fixed point
+
+        if (t >= m_nSpectrumPoints)
+        {
+            wave_packets.push_back(std::vector < complex < double > > (wave_packet));
+        }
+
+        // if enough data collected at the fixed point,
+        // calculate fourier transform and get a list
+        // of frequencies to watch for later to reconstruct
+        // spatial wave functions
+
+        if (wave_packets.size() >= m_nSpectrumPoints)
+        {
+            spectrum.resize(wave_packets.size());
+            for (size_t i = 0; (i < wave_packets.size()) && m_bWorking; ++i)
+            {
+                spectrum[i] = 0;
+                for (size_t j = 0; (j < wave_packets.size()) && m_bWorking; ++j)
+                {
+                    spectrum[i] = spectrum[i] + wave_packets[j][fixed_point]
+                        * exp(- _i * (double)i / wave_packets.size() * (1. / m_lfTimeDelta / 2)
+                              * ((int)t - (int)wave_packets.size() + j) * m_lfTimeDelta);
+                }
+                spectrum[i] = spectrum[i] / fixed_point / m_lfDistanceDelta;
+            }
+
+            double mean_spectrum = 0;
+            std::vector < double > spectrum_norm(spectrum.size());
+            for (size_t i = 0; i < spectrum.size(); ++i)
+            {
+                spectrum_norm[i] = sqnorm(spectrum[i]);
+                mean_spectrum += spectrum_norm[i];
+            }
+
+            mean_spectrum /= spectrum.size();
+
+            energy_levels.clear();
+
+            bool has_any;
+            do
+            {
+                double max_val = 0;
+                size_t max_idx = 0;
+
+                // find a neighborhood of the maximum
+
+                for (size_t i = 0; i < spectrum.size(); ++i)
+                {
+                    if (spectrum_norm[i] > max_val)
+                    {
+                        max_val = spectrum_norm[i];
+                        max_idx = i;
+                        has_any = true;
+                    }
+                }
+
+                if (max_val < mean_spectrum * 2) has_any = false;
+
+                if (!has_any) break;
+
+                energy_levels.push_back((double) max_idx / spectrum.size()
+                                        * (1 / m_lfTimeDelta / 2));
+                energy_level_indices.push_back(max_idx);
+
+                // cut a neighborhood of the maximum
+
+                double thr_r = max_val, thr_l = max_val;
+                size_t peak_width_r = 0, peak_width_l = 0;
+
+                for (size_t i = max_idx + 1; i < spectrum.size(); ++i)
+                {
+                    if (spectrum_norm[i] <= thr_r)
+                    {
+                        thr_r = spectrum_norm[i];
+                        ++peak_width_r;
+                        continue;
+                    }
+                    break;
+                }
+
+                for (size_t i = max_idx; i-- > 0;)
+                {
+                    if (spectrum_norm[i] <= thr_l)
+                    {
+                        thr_l = spectrum_norm[i];
+                        ++peak_width_l;
+                        continue;
+                    }
+                    break;
+                }
+
+                for (size_t i = max_idx; i < min(max_idx + peak_width_r * 3, spectrum.size()); ++i)
+                {
+                    spectrum_norm[i] = 0;
+                }
+
+                for (size_t i = max(0, (int)max_idx - (int)peak_width_l * 3); i < max_idx; ++i)
+                {
+                    spectrum_norm[i] = 0;
+                }
+            } while (has_any && (energy_levels.size() < max_energy_levels));
+
+            // refine energy levels found
+
+            auto spectrum_fn = [&] (double e) -> double
+            {
+                complex < double > val = 0;
+                for (size_t j = 0; (j < wave_packets.size()) && m_bWorking; ++j)
+                {
+                    val = val + wave_packets[j][fixed_point]
+                        * exp(- _i * e * ((int)t - (int)wave_packets.size() + j) * m_lfTimeDelta);
+                }
+                return norm(val);
+            };
+
+            // apply gold section method
+
+            const double gs = 0.618;
+            for (size_t i = 0; i < energy_levels.size(); ++i)
+            {
+                double e = energy_levels[i];
+                double left_e = e - (1 / m_lfTimeDelta / wave_packets.size() / 2);
+                double right_e = e + (1 / m_lfTimeDelta / wave_packets.size() / 2);
+                double e1 = right_e - (right_e - left_e) * gs;
+                double e2 = left_e + (right_e - left_e) * gs;
+                double val1 = spectrum_fn(e1), val2 = spectrum_fn(e2);
+                do
+                {
+                    if (val1 <= val2)
+                    {
+                        left_e = e1;
+                        e1 = e2;
+                        e2 = left_e + (right_e - left_e) * gs;
+                        val2 = spectrum_fn(e2);
+                    }
+                    else
+                    {
+                        right_e = e2;
+                        e2 = e1;
+                        e1 = right_e - (right_e - left_e) * gs;
+                        val1 = spectrum_fn(e1);
+                    }
+                } while ((right_e - left_e) > 1e-8);
+                energy_levels[i] = (right_e + left_e) / 2;
+            }
+
+            spectrum_plot.refresh();
+            m_cSpectrumPlot.RedrawBuffer();
+            m_cSpectrumPlot.SwapBuffers();
+            Invoke([&] () {
+                m_cSpectrumPlot.RedrawWindow();
+            });
+
+            if (!energy_levels.empty())
+            {
+                for (size_t i = 0; i < max_wavefuncs; ++i)
+                {
+                    wavefunc_plots[i].data->container_ptr->clear();
+                }
+                for (size_t i = 0; i < energy_levels.size(); ++i)
+                {
+                    wavefunc_plots[i].data->container_ptr->resize(n);
+                }
+
+                for (size_t k = 0; k < n; ++k)
+                {
+                    for (size_t i = 0; i < energy_levels.size(); ++i)
+                    {
+                        complex < double > val = 0;
+                        if (k != 0)
+                        {
+                            for (size_t j = 0; (j < wave_packets.size()) && m_bWorking; ++j)
+                            {
+                                val = val + wave_packets[j][k]
+                                    * exp(- _i * energy_levels[i]
+                                          * ((int)t - (int)wave_packets.size() + j) * m_lfTimeDelta);
+                            }
+                            val = val / k;
+                        }
+                        wavefunc_plots[i].data->container_ptr->at(k) = val;
+                    }
+                }
+
+                wavefunc_plots[0].auto_world->clear();
+                if (m_nWaveFunctionToDisplay > energy_levels.size())
+                {
+                    m_nWaveFunctionToDisplay = energy_levels.size();
+                }
+                if (m_nWaveFunctionToDisplay <= 0)
+                {
+                    for (size_t i = 0; i < energy_levels.size(); ++i)
+                    {
+                        wavefunc_plots[i].view->visible = true;
+                        wavefunc_plots[i].auto_world->adjust(*wavefunc_plots[i].data);
+                    }
+                }
+                else
+                {
+                    for (size_t i = 0; i < energy_levels.size(); ++i)
+                    {
+                        wavefunc_plots[i].view->visible = ((i + 1) == m_nWaveFunctionToDisplay);
+                        if ((i + 1) == m_nWaveFunctionToDisplay)
+                        {
+                            wavefunc_plots[i].auto_world->adjust(*wavefunc_plots[i].data);
+                        }
+                    }
+                }
+                wavefunc_plots[0].auto_world->flush();
+                m_cWaveFunctionPlot.RedrawBuffer();
+                m_cWaveFunctionPlot.SwapBuffers();
+                Invoke([&] () {
+                    m_cWaveFunctionPlot.RedrawWindow();
+                });
+            }
+
+            wave_packets.clear();
+        }
+
+        ++t;
+
+        wave_packet_plot.refresh();
+        m_cWavePacketPlot.RedrawBuffer();
+        m_cWavePacketPlot.SwapBuffers();
+        Invoke([&] () {
+            m_cWavePacketPlot.RedrawWindow();
+        });
+    }
+
     CSimulationDialog::OnSimulation();
 }
 
